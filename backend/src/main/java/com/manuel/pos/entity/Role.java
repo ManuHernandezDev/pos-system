b3c856f6a6d95ead5f3f9f5ec1b27ea0568df642
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-04-30 21:40:21 -0600

    Initial porject Structure (backend + frontend)

---
+++ backend/src/main/java/com/manuel/pos/entity/Role.java
@@ -0,0 +1,18 @@
+package com.manuel.pos.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ backend/src/test/java/com/manuel/pos/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
